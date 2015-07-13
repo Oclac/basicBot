99ff942c40be2d0c909f7303831e36f4165ba2bc
Oclac <ruudtognimail@gmail.com>
2015-07-13 16:39:08 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2175,20 +2175,6 @@
                 }
             },
 
-             resdjcommand: {
-                command: 'rdj',
-                rank: 'user',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                   if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        API.sendChat("Interested in applying for Resident DJ? Visit here: https://www.nightcore-331.net/viewforum.php?f=43 and make sure you meet all of the requirements first!C")
-                        }
-                    }
-                }
-            },
-    
             forceskipCommand: {
                 command: ['forceskip', 'fs'],
                 rank: 'bouncer',
