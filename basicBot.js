8e7c35483ee4a1b91dc6292340c7427b0fb2956b
Oclac <ruudtognimail@gmail.com>
2015-08-05 23:36:58 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -9,7 +9,7 @@
 
     /*window.onerror = function() {
         var room = JSON.parse(localStorage.getItem("basicBotRoom"));
-        window.location = 'https://52.7.249.10' + room.name;
+        window.location = 'https://54.88.12.141' + room.name;
     };*/
 
     API.getWaitListPosition = function(id){
