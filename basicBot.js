b3237cc539a011d8f215b3b706876ba437b39fbe
Oclac <ruudtognimail@gmail.com>
2017-02-27 11:37:24 +0100

    Waifu <3
    Updated the name

--- basicBot.js
+++ basicBot.js
@@ -248,7 +248,7 @@
         retrieveSettings: retrieveSettings,
         retrieveFromStorage: retrieveFromStorage,
         settings: {
-            botName: "basicBot",
+            botName: "TrgWaifuBot",
             language: "english",
             chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
             scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
